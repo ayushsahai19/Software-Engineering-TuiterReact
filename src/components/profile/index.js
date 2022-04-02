11bfd2c97359f4f9ee6a6bd6a5e973eaa9c74227
ayushsahai19 <ayushsahai96@gmail.com>
2022-04-02 15:43:45 -0400

    Reverting to old branch

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -97,7 +97,6 @@ const Profile = () => {
         </div>
       </div>
         <Routes>
-          
           <Route path="/mytuits" element={<MyTuits/>}/>
           <Route path="/tuits-and-replies" element={<TuitsAndReplies/>}/>
           <Route path="/media" element={<Media/>}/>

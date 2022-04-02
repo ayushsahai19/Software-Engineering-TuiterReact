f87064fb9179ed2e23c44bf4600b0a88d8bfe11b
ayushsahai19 <ayushsahai96@gmail.com>
2022-04-01 23:32:11 -0400

    added dislikes button

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const TuitStats = ({tuit, likeTuit = () => {}}) => {
+const TuitStats = ({tuit, likeTuit, dislikeTuit = () => {}}) => {
     return (
       <div className="row mt-2">
         <div className="col">
@@ -21,10 +21,18 @@ const TuitStats = ({tuit, likeTuit = () => {}}) => {
           </span>
         </div>
         <div className="col">
-          <span className="fa-solid fa-thumbs-down" onClick={() => likeTuit(tuit)}>
-              <span className="ttr-stats-likes">{tuit.stats && tuit.stats.likes}</span>
+        <span onClick={() => dislikeTuit(tuit)}>
+          {
+            tuit.stats && tuit.stats.dislikes > 0 &&
+            <i className="far fa-thumbs-down fa-solid me-1" />
+          }
+          {
+            tuit.stats && tuit.stats.dislikes <= 0 &&
+            <i className="far fa-thumbs-down me-1"/>
+          }
+          {tuit.stats && tuit.stats.dislikes}
           </span>
-        </div>
+      </div>
         <div className="col">
           <i className="far fa-inbox-out"></i>
         </div>

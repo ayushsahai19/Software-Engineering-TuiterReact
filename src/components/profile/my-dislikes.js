e5e8ae631413d45dd9e4c3b8288628a9643f799b
ayushsahai19 <ayushsahai96@gmail.com>
2022-04-02 02:55:33 -0400

    my dis-likes screen profile added

--- src/components/profile/my-dislikes.js
+++ src/components/profile/my-dislikes.js
@@ -1,8 +1,18 @@
+import Tuits from "../tuits";
+import * as service from "./../../services/dislikes-service";
+import {useEffect, useState} from "react";
+
 const MyDislikes = () => {
-    return(
-        <div>
-            <h1>My Dislikes</h1>
-        </div>
-    )
-}
+  const [dislikedTuits, setDislikedTuits] = useState([]);
+  const findTuitsIDislike = () => {
+    service.findAllTuitsDislikedByUser("me").then((tuits) => setDislikedTuits(tuits));
+  }
+  useEffect(findTuitsIDislike, []);
+
+  return(
+    <div>
+      <Tuits tuits={dislikedTuits} refreshTuits={findTuitsIDislike}/>
+    </div>
+  );
+};
 export default MyDislikes;
